0e698cf224690eb0c60ecc2d68d78eb02e94402f
Howard Zhang <zamhown@live.com>
2021-06-16 03:07:13 +0800

    add a build script

---
+++ dist.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# 将代码构建到uncanny-valley-poetry-dist仓库中
+
+rm -rf uncanny-valley-poetry-dist/static
+
+mv .env.production assets/.env.production.1
+mv assets/.env.production.2 .env.production
+npm run build
+mv .env.production assets/.env.production.2
+mv assets/.env.production.1 .env.production
+
+\cp -rf build/. uncanny-valley-poetry-dist
